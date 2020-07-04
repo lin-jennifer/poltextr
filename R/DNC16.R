7da23776abfe51f59af3494d2fe040539a8b07de
Jennifer Lin <jennifer.lin16@ncf.edu>
2020-07-04 09:30:49 -0400

    dnc16 corpus

--- R/DNC16.R
+++ R/DNC16.R
@@ -12,6 +12,16 @@ dnc16raw <- dir(
   recursive  = TRUE)        # Repeat
 
 library("readtext")
-dnc16 <- corpus(readtext(dnc16raw))
+library(tidyverse)
+dnc16 <- corpus(readtext(dnc16raw), docid_field = "doc_id")
 summary(dnc16)
-save(dnc16, file = here::here("data/dnc16.Rda"))
+save(dnc16, file = here::here("data/dnc16.RData"),version = 2)
+
+# ***********************************
+# NOTES #
+# ***********************************
+
+# For conversion to data frame
+
+# dnc16 <- data.frame(text = sapply(dnc16, as.character), stringsAsFactors = FALSE)
+# dnc16 <- tibble::rownames_to_column(dnc16, "document")
